7fcb84ed408747aca456d5bae817cc57b8bc0ce5
Apple <sndpjaat008@gmail.com>
2024-08-16 09:53:22 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+  gsap.to("#text1", {
+    opacity: 0,  
+    x: 200,   
+    duration: 2, 
+    delay: 0.5,    
+    repeat: -1,
+    yoyo: true    
+  });
+  gsap.from("#text2", {
+    opacity: 0,    
+    rotate:"360",
+    y: -50,      
+    xPercent:-440,
+    duration: 1.5, 
+    delay: 1,      
+    repeat: -1,   
+    yoyo: true    
+  });
+  gsap.fromTo("#text3",
+    { opacity: 0, y: 50 , },  
+    { opacity: 1, y: 0, duration: 6, delay: 1.5, repeat: -1, yoyo: true,  background:"orange" ,    xPercent:340, }  
+  );
+  gsap.fromTo("#text4",
+    { opacity: 0, y: 50 , }, 
+    { opacity: 1, y: 0, duration: 6, delay: 1.5, repeat: -1, yoyo: true, background:"green" ,  xPercent:-240, } 
+  );
+  gsap.fromTo("#text5",
+    { opacity: 0, y: 50 , },  
+    { opacity: 1, y: 0, duration: 6, delay: 1.5, repeat: -1, yoyo: true,  background:"orange" , rotate:-"560",   xPercent:340, }  
+  );
+//   gsap.fromTo("#text6",
+//     { opacity: 0, y: 50 , }, 
+//     { opacity: 1, y: 0, duration: 6, delay: 1.5, repeat: -1, yoyo: true, background:"green" ,  xPercent:-240, } 
+//   );
+  
+    gsap.fromTo("#text6",
+        { opacity: 0,  
+            x: 200,   
+            duration: 2, 
+            delay: 0.5,    
+            repeat: -1,
+            yoyo: true  } 
+      );
